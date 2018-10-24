1622b55b1fcfa73e6988b7d23c221adefe0b9be3
Ryan Coffee <coffeer76@gmail.com>
2018-10-23 17:04:28 -0700

    incremental: reverseing handedness of calibration delay

--- include/CalibMat.hpp
+++ include/CalibMat.hpp
@@ -19,7 +19,7 @@ class CalibMat {
 		bool print_delays(std::ofstream & out);
 		inline void set_fsWindow(const double x = 3333){fsWindow = x;}
 
-		inline double get_delay(const size_t i){ return fsCenter + fsWindow * (double(i)/double(ndelays)-0.5); }
+		inline double get_delay(const size_t i){ return fsCenter - fsWindow * (double(i)/double(ndelays)-0.5); }
 		inline size_t get_ndelays(void){return ndelays;}
 		inline size_t set_ndelays(const size_t n){ndelays = n; return ndelays;}
 		inline void set_center(const double x = -3000){fsCenter = x;}
