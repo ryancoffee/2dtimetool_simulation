1d8436759532043f113e493d36ebfa83010aea96
Ryan Coffee <coffeer76@gmail.com>
2019-01-23 11:11:40 -0800

    Incremental
    OK, now I have a decay term set to the maximum vector length
    This allows for subtracting the decay from the carrier cascade rate to bring the
    population back to zero before the end of the vector, that way earlier than
    t0 we have a flat zero carrier integral (fourier_integral)

--- src/simcascade.py
+++ src/simcascade.py
@@ -13,17 +13,39 @@ def diamond_cascade(x):
     result = 0.5*(1.+erf(x/wrise))*(y0+slope*x)*.5*(erfc((x-xfall)/wfall))
     return result
 
-def fourier_shift(x,t=0.):
-    X=fft(x)
-    F=fftfreq(x.shape[0])
-    X *=np.exp(-1j*2.*np.pi*F*t)
-    return np.real(ifft(X))
+def integrate(x):
+    _y = np.cumsum(x)
+    return _y
+
+def fourier_shift(x,t=0.,alpha=1e-4):
+    _X=fft(x)
+    _F=fftfreq(x.shape[0])
+    _alpha = alpha if len(x)>int(1./alpha) else 1./(len(x))
+    _denom = (_alpha+1j*_F)
+    _denom = np.power(_denom,int(-1))
+    _denom *=np.exp(-1j*2.*np.pi*_F*(x[0]))
+    _X *=np.exp(-1j*2.*np.pi*_F*t)
+    _X *= _denom
+    return np.real(ifft(_X))
 
 def fourier_shift_integrate(x,t=0.):
     X=fft(x)
     F=fftfreq(x.shape[0])
-    X *=np.exp(-1j*2.*np.pi*F*t)
-    denom = 1j*F
+    X *=np.exp(-1j*2.*np.pi*F*t) # time shift
+    denom = 1j*F # integrate
+    denom[1:] = np.power(denom[1:],int(-1))
+    denom[0]=0
+    result = np.real(ifft(X*denom))
+    result -= np.min(result)
+    result /= np.max(result)
+    return result
+
+def fourier_shift_decay_integrate(x,t=0.,alpha=10e-3):
+    X=fft(x)
+    _alpha = alpha if len(x)>int(1./alpha) else 1./(len(x))
+    F=fftfreq(x.shape[0])
+    X *=np.exp(-1j*2.*np.pi*F*t) #time shift
+    denom = 1j*F*(_alpha+1j*F)
     denom[1:] = np.power(denom[1:],int(-1))
     denom[0]=0
     result = np.real(ifft(X*denom))
@@ -32,12 +54,17 @@ def fourier_shift_integrate(x,t=0.):
     return result
 
 def main():
-    xvals = np.arange(-10,1000000)
-    np.savetxt('testcascade.out',diamond_cascade(xvals),fmt='%.4f')
-    yvals = fourier_shift(diamond_cascade(xvals),10)
-    np.savetxt('testcascade.shifted.out',yvals,fmt='%.4f')
-    yvals = fourier_shift_integrate(diamond_cascade(xvals),0)
-    np.savetxt('testcascade.integrated.out',yvals,fmt='%.4f')
+    span = 1e4
+    xvals = np.arange(-span//100,99*span//100,dtype=float)
+    yvals = diamond_cascade(xvals)
+    dvals = integrate(yvals)
+    dvals *= np.sum(yvals)/np.sum(dvals)
+    print(np.sum(yvals-dvals))
+    out = np.column_stack((xvals,yvals,dvals))
+    newyvals = yvals-dvals
+    np.savetxt('./data_container/raw/testcascade.out',out,fmt='%.4f')
+    out = np.column_stack((xvals,yvals,dvals,fourier_shift_integrate(yvals),fourier_shift_integrate(newyvals)))
+    np.savetxt('./data_container/raw/testcascade.integrated.out',out,fmt='%.4f')
 
 
     return 0
