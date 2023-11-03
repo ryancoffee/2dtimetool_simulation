0c4264390b597aa18f0b69d5d65769c4b2e0b8b9
Ryan Coffee <coffeer76@gmail.com>
2023-11-02 23:52:16 -0700

    test probat

--- src/scan_material.cpp
+++ src/scan_material.cpp
@@ -8,7 +8,7 @@
 
 #include <cstdint> // for sake of defining uint16_t for the OpenCV mat to be filled.
 // OpenCV includes
-#include <opencv2/core.hpp>
+#include <opencv4/opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/imgcodecs.hpp>
